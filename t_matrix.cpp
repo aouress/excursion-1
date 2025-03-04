fead44cb50c046cff6a9bc985794ee96c203a3d4
Aiden Shepler <shepleraiden@gmail.com>
2025-03-04 02:20:11 -0500

    Added functions to parse line
    The functions parse a single line and find the desired value based on the function.

--- t_matrix.cpp
+++ t_matrix.cpp
@@ -1,28 +1,91 @@
 #include <iostream>
-#include <fstream>
-#include <string> 
-#include <vector>
-// fstream documentation | https://cplusplus.com/reference/fstream/
+#include <fstream> // fstream documentation | https://cplusplus.com/reference/fstream/
+#include <string>  // string documentation  | https://cplusplus.com/reference/string/
+#include <vector>  // vector documentation  | https://cplusplus.com/reference/vector/
+#include <cstdlib> // cstdlib documentation | https://cplusplus.com/reference/cstdlib/
+
 
 using namespace std;
+
+int get_sord (string line, char code) {
+    // given a line and a code return the desired float number
+    // code 's': source 
+    // code 'd': destination
+
+    fstream netlist;
+    // size_t is the find method's return data type 
+    size_t start = 0, end; 
+    string sub; 
+
+    netlist.open("netlist.txt", ios::in); 
+
+    if (code == 's') {
+        
+        // first space is between component type and source node so skip 
+        end = line.find(' ', start);     
+        // iterate once through start and end values so that they are between the source node 
+        start = end + 1;
+        end = line.find(' ', start);
+        sub = line.substr(start, end);   
+        
+    }
+    else if (code == 'd') {
+        end = line.find(' ', start); 
+
+        // increment twice to get start and end between the desintation node
+        for (int i = 0; i < 2; i++) {
+            start = end + 1; 
+            end = line.find(' ', start); 
+        }
+        sub = line.substr(start, end); 
+    }
+    else {
+        cout << "Invalid code for finding source or destination node" << endl; 
+    }
+    netlist.close();
+    return stoi(sub);
+}
+
+char get_type (string line) {
+    // return 'V' if voltage source and 'R' if resistor
+
+    // the type will always be in the first position of the line, so just return that char
+    return line[0]; 
+}
+
+float get_value(string line) {
+    //returns the value of the circuit element as a float
+
+    fstream netlist;
+    size_t start = 0, end; 
+    string sub; 
+
+    netlist.open("netlist.txt", ios::in); 
+    end = line.find(' ', start); 
+
+    // increment three times to get start and end between the element's value 
+    for (int i = 0; i < 3; i++) {
+        start = end + 1; 
+        end = line.find(' ', start); 
+    }
+    sub = line.substr(start, end); 
+    
+    netlist.close();
+    
+    // use cstdlib to convert from c-string to float 
+    return atof(sub.c_str());
+}
+
 
 //finds the source node and compares to previous max 
 int find_max_source_node(string line, int max_node) {
     
-    // size_t is the find method's return data type 
-    //http://cplusplus.com/reference/string/string/find/
-    size_t start = 0, end;
-
-    // first space is between component type and source node so skip 
-    end = line.find(' ', start); 
-    //second space is between source node and destination node, giving length of source node for indexing
-    start = end + 1;
-    end = line.find(' ', start);
-    string sub = line.substr(start, end);
+    // finds source node and returns as int
+    int s_node = get_sord(line, 's'); 
 
     //new max node found if equality holds 
-    if (stoi(sub) > max_node) {
-        max_node = stoi(sub); 
+    if (s_node > max_node) {
+        max_node = s_node;  
     }
     return max_node; 
 }
@@ -56,14 +119,14 @@ void read_file() {
     }
     //calculate dimension of T matrix | row: # of rows; col: # of cols
     // Including the solution column, the dimension of the augmented matrix of [T][e] = [0], [T | 0],
-    // row # of rows and row + 1 # of columns
-    int row = 2*length + max_node; 
+    // row # of rows and row + 1 # of columns 
+    int row = 2*length + max_node;    
     int col = row + 1; 
     //----- 
  
     // Declares a nested vector that has row number of row vectors to form the [T | u]
     // augmented matrix
-    vector<vector<int>> Tu(row, vector<int>(col, 0)); 
+    vector<vector<float>> Tu(row, vector<float>(col, 0)); 
 
     //make zeros matrix 
     for (int i = 0; i < row; i++) {
