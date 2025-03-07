dfa69a1736c0cce848e4a86af8b3ae0f63627ea2
Aiden Shepler <shepleraiden@gmail.com>
2025-03-06 20:23:59 -0500

    Matrix solving algorithm finished
    I finished the matrix solving algorithm and changed the 2D vector type to double for increased precision.
    
    Currently, the code prints the initial matrix, the row echelon form of the matrix, the reduced row echelon form of the matrix, and the solution vector.
    
    Next, I want to organize all the matrix solving functions into a single header file for better modularization

--- t_matrix.cpp
+++ t_matrix.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void insertMMatrix(vector<vector<float>>& T, int max_node, int length) {
+void insertMMatrix(vector<vector<double>>& T, int max_node, int length) {
     int startRow = max_node+length;  //The index of the row is after max_node and length
     int startCol = max_node;         //Places identity matrix to the right by the size of max_node
 
@@ -18,7 +18,21 @@ void insertMMatrix(vector<vector<float>>& T, int max_node, int length) {
     }
 }
 
-int solve_matrix(vector<vector<float>> &T, int max_node, int length); 
+void back_sub(vector<vector<double>> &Tu, int row, int col); 
+
+void interchange_row(vector<vector<double>> &Tu, int row_1, int row_2); 
+
+void ref_matrix(vector<vector<double>> &Tu, int max_node, int length, int row, int col); 
+
+void rref_matrix(vector<vector<double>> &Tu, int row, int col);
+
+void scale_replace_row (vector<vector<double>> &Tu, int row_1, int row_2, int current_col, int max_col);
+
+void matrix_threshold (vector<vector<double>> &Tu, int row, int col);
+
+bool valid_matrix(vector<vector<double>> &Tu, int row, int col); 
+
+int matrix_rank(vector<vector<double>> &Tu, int row, int col);
 
 //finds the source node and compares to previous max 
 int find_max_source_node(string line, int max_node) {
@@ -34,12 +48,13 @@ int find_max_source_node(string line, int max_node) {
 }
 
 //given a netlist file, read and write the data into appropriate location in the matrix 
-void read_file() {
+int read_file() {
     fstream netlist;
     int length = 0;
     int max_node = 0; 
 
     //----
+
     // determine matrix dimensions (could be separate function)
     netlist.open("netlist.txt", ios::in); //read file
     if (netlist.is_open()) {
@@ -65,13 +80,54 @@ void read_file() {
     // row # of rows and row + 1 # of columns 
     int row = 2*length + max_node;    
     int col = row + 1; 
+
     //----- 
- 
+    
     // Declares a nested vector that has row number of row vectors to form the [T | u]
     // augmented matrix
-    vector<vector<float>> Tu(row, vector<float>(col, 0)); 
+    vector<vector<double>> Tu(row, vector<double>(col, 0)); 
 
-    insertMMatrix(Tu, max_node, length); 
+    Tu = {
+        {2, 3, -1, 4, 5, 0, -2, 1, 7},
+        {1, -2, 3, -4, 5, -6, 7, -8, 9},
+        {0, 5, -2, 3, 1, -4, 6, 2, -3},
+        {4, -1, 2, -5, 3, 6, -7, 8, 0},
+        {3, 2, -4, 1, -6, 5, -7, 9, -8},
+        {-2, 4, -1, 3, -5, 2, 6, -8, 7},
+        {5, -3, 2, -1, 4, -7, 8, -6, 9},
+        {1, 6, -5, 3, -2, 7, -4, 0, 8}
+    };
+
+    //insertMMatrix(Tu, max_node, length); 
+
+    // output rows 
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j ++) {
+            cout << Tu[i][j] << " "; 
+        }
+        cout << endl;
+    }  
+    cout << endl; 
+
+    //compute reduced echelon form of the matrix
+    ref_matrix(Tu, max_node, length, row, col); 
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j ++) {
+            cout << Tu[i][j] << " "; 
+        }
+        cout << endl;
+    }  
+    cout << endl; 
+
+    // check if the matrix has a unique solution 
+    if (!valid_matrix(Tu, row, col)) {
+        cout << "The netlist provided describes an invalid circuit configuration" << endl; 
+        return 0;
+    }
+
+    // compute the reduced row echelon form of the matrix
+    rref_matrix(Tu, row, col); 
 
     // output rows 
     for (int i = 0; i < row; i++) {
@@ -80,12 +136,162 @@ void read_file() {
         }
         cout << endl;
     }
+    cout << endl;
+
+    vector<double> solution_vector(row); 
+
+    for (int i = 0; i < row; i ++) {
+        solution_vector[i] = Tu[i][col-1]; 
+        cout << solution_vector[i] << " ";
+    }
+    cout << endl;
+    return 1;
 }
 
 int main() {
     read_file();
 }
 
-int solve_matrix(vector<vector<float>> &T, int max_node, int length) {
-    //iterate through each row. Find a nonzero pivot in the column. increment a pivot row counter.  
+void interchange_row (vector<vector<double>> &Tu , int row_1, int row_2) {
+    // swap row 1 and row 2 of the 2D vector matrix 
+    vector<double> temp = Tu[row_1]; 
+    Tu[row_1] = Tu[row_2]; 
+    Tu[row_2] = temp; 
+
+}
+
+void scale_replace_row (vector<vector<double>> &Tu, int row_1, int row_2, int current_col, int max_col) {
+    // find a scalar multiple, c, such that the elements in col sum to zero 
+    // replace row_2 such that c*row_1 + row_2
+    double c = -Tu[row_2][current_col]/Tu[row_1][current_col]; 
+
+    //apply element wise scalar multiplication to row_2
+    for (int i = 0; i < max_col; i++) {
+        Tu[row_2][i] = c*Tu[row_1][i] + Tu[row_2][i]; 
+    }
+}
+
+void scale_row (vector<vector<double>> &Tu, int row, int col, int max_col) {
+    // scales the row such that the scalar multiple is the reciprocal of the row index
+    double c = Tu[row][col]; 
+
+    for (int i = 0; i < max_col; i++) {
+        Tu[row][i] = Tu[row][i]/c; 
+    }
+}
+
+void ref_matrix(vector<vector<double>> &Tu, int max_node, int length, int row, int col) {
+    //function to compute the reduced echelon form of the [T | u] augmented matrix 
+    
+    // counter to store how many pivots we've placed 
+    int piv_row = 0;
+
+    // the counter incremented when first nonzero element is swapped and also serves as a check to see if we've already swapped rows in the column 
+    int nonzero_count = 0; 
+
+    //iterate through each column and search for a nonzero element in the rows, then clear the remaining elements in the column 
+    // the last column of the Tu matrix corresponds to the solution vector, so we do not need to iterate over it
+    for (int j = 0; j < col-1; j ++) {
+        for (int i = 0; i < row; i++) {
+            // row interchange
+            // find non zero element
+            // if i is larger than or equal to the piv_row it will either interchange the same row, 
+            // or interchange the ith row with the piv_row row. 
+            if (Tu[i][j] != 0 && i >= piv_row && nonzero_count == 0) {
+                interchange_row(Tu, i, piv_row); 
+
+                // column has at least one nonzero element
+                nonzero_count += 1;
+                // nonzero element in correct pivot position 
+                piv_row += 1;
+            }
+
+            // clear remaining nonzero elements from the column so long as a row has been interchanged
+            // since we've already found the pivot for this column, we can assume all other 
+            // nonzero elements come after the initial position of the first nonzero element 
+            if(Tu[i][j] != 0 && nonzero_count == 1 && i != piv_row - 1) {
+                scale_replace_row(Tu, piv_row - 1, i, j, col); 
+            }
+        }
+        nonzero_count = 0;
+    }
+
+    matrix_threshold(Tu, row, col);
+}
+
+int matrix_rank(vector<vector<double>> &Tu, int row, int col) {
+    // the rank of a matrix is the number of pivots in the matrix 
+    // this function counts the number of pivots and returns it as the rank
+
+    // for each row, iterate through the columns until you see a nonzero value 
+    // when you see a nonzero value, increment the pivot counter
+    int pivot = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            
+            if (Tu[i][j] != 0 && i >= pivot) {
+                // nonzero element in correct pivot position 
+                pivot += 1;
+            }
+        }
+    }
+    return pivot;
+}
+
+bool valid_matrix (vector<vector<double>> &Tu, int row, int col) {
+    
+    // if the rank of the coefficient matrix T is equal to the rank of the augmented matrix [ T | u ] the circuit is well defined 
+    // essentially, this means we have no rows of the form [0 ... 0 | b ], so the system has a unique solution.
+    if (matrix_rank(Tu, row, col) == matrix_rank(Tu, row, col-1)) {
+        return true;
+    }
+    return false;
+}
+
+void matrix_threshold (vector<vector<double>> &Tu, int row, int col) {
+    // upon testing the function with an arbitrary matrix, some values that should be exactly zero (and are very close to being zero; e.g., 1e^-9) are not zero. 
+    // this final portion of the function checks a threshold value and assigns very very small values to 0.0 
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j ++) {
+            if (abs(Tu[i][j]) < 1e-9) {
+                Tu[i][j] = 0.0;
+            }
+    }
+    }
+}
+
+void back_sub(vector<vector<double>> &Tu, int row, int col) {
+    // assume the matrix is in ref form with all pivots along the main diagonal of the coefficient matrix
+
+    int piv_row = row - 1; 
+    int piv_col = col - 2;
+
+    //start in bottom corner of coefficient matrix 
+    for (int j = col - 2; j >=0; j--) {
+        for (int i = piv_row - 1; i >= 0; i--) {
+            // scale and add the pivot row to each row whose column element is nonzero 
+            // this adjusts all upper triangular matrix elements to zero 
+            if (Tu[i][j] != 0) {
+                scale_replace_row(Tu, piv_row, i, j, col);
+            }
+                 
+        }
+        //scale row to get a 1 in the pivot position  
+        scale_row(Tu, piv_row, j, col);
+        // decrement pivot row and piv col to move diagonally up the matrix
+        piv_row -= 1;
+        piv_col -= 1;
+
+    }
+    matrix_threshold(Tu, row, col); 
+}
+
+void rref_matrix(vector<vector<double>> &Tu, int row, int col) {
+
+    // check if the matrix has a unique solution 
+    if (!valid_matrix(Tu, row, col)) {
+        cout << "The netlist provided describes an invalid circuit configuration" << endl; 
+    }
+    // back sub the ref matrix to obtain the rref matrix
+    back_sub(Tu, row, col); 
 }
