19fa485282fb7e4f754acd66379ef2b4c01034f4
michaelhemingway <hemingway.mp@gmail.com>
2025-03-08 12:17:53 -0500

    Mu matrix function
    Function to add mu matrix to the tu matrix

--- t_matrix.cpp
+++ t_matrix.cpp
@@ -5,23 +5,67 @@
 #include <string>  // string documentation  | https://cplusplus.com/reference/string/
 #include <vector>  // vector documentation  | https://cplusplus.com/reference/vector/
 #include <cstdlib> // cstdlib documentation | https://cplusplus.com/reference/cstdlib/
+#include <sstream> // sstream documentation | https://cplusplus.com/reference/sstream/stringstream/
 
 using namespace std;
 
 void insertMMatrix(vector<vector<float>>& Tu, int max_node, int length) {
-    int startRow = max_node+length;  //The index of the row is after max_node and length
-    int startCol = max_node;         //Places identity matrix to the right by the size of max_node
+    int startRow = max_node+length; //The index of the row is after max_node and length
+    int startCol = max_node; //Places identity matrix to the right by the size of max_node
 
     for (int i = 0; i<length; i++) {
         Tu[startRow+i][startCol+i] = 1; //Diagonally adds 1s
     }
 
-    int startRow = max_node;  //The index of the row is after max_node
-    int startCol = max_node;         //Places identity matrix to the right by the size of max_node
+    startRow = max_node; //The index of the row is after max_node
+    startCol = max_node; //Places identity matrix to the right by the size of max_node
     
     //Adds an identity matrix in the center
     for (int i = 0; i<length; i++) {
         Tu[startRow+i][startCol+i] = 1; //Diagonally adds 1s
+    }
+}
+
+void addMu(vector<vector<float>>& Tu, int max_node, int length) {
+    
+    //Reading the text file and storing voltages
+    string stream; //String to hold the data in the text file
+    ifstream MyReadFile("netlist.txt"); //reading the text file
+
+    vector<double> voltages; //vector to store voltages in the string
+
+    //Reading the file line by line
+    while (getline(MyReadFile, stream)) {
+        istringstream iss(stream);
+        string component;
+        int node1, node2;
+        double value;
+
+        // Extract values from line
+        if (iss >>component >>node1 >>node2 >>value) {
+            //If the component starts with 'V', then save the value
+            if (component[0] == 'V') {
+                voltages.push_back(value);
+            }
+            //If not, place a '0' as a placeholder
+            else {
+                voltages.push_back(0);
+            }
+        }
+    }
+
+    //Close the file
+    MyReadFile.close();
+
+    //Determining where the muS matrix is in the tu matrix
+    int startRow = max_node+length;  //The index of the row is after max_node and length
+    int startCol = max_node+length+length; //Places identity matrix to the right by the size of max_node and two lengths
+    int i=0; //to iterate through the matrix rows
+
+    //Add voltages to muS, iterating through the voltage vector
+    for (double v : voltages) {
+        Tu[startRow+i][startCol] = v;
+        i++;
     }
 }
 
@@ -93,9 +137,10 @@ void read_file() {
 
     //Inserting the m-matrix
     insertMMatrix(Tu, max_node, length);
+    addMu(Tu, max_node, length);
 
     //Showing the matrix after inserting m-matrix
-    cout << "\nT matrix with M-matrix:" << endl;
+    cout << "\nT matrix with M, N and mu-matrix,:" << endl;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             cout << Tu[i][j] << " "; 
